f62d6477aa2fec993fca9c7480800b0c9ec93780
Loredana R <loredana.lehadus@yahoo.com>
2018-07-31 00:50:17 +0300

    added endpoint that gets all contacts, defaulted to limit 2

---
+++ MysteryBox.Web/Models/Contacts.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace MysteryBox.WebService.Models
+{
+    public static class Contacts
+    {
+        public static IEnumerable<Contact> StoredContacts = new List<Contact>
+        {
+            new Contact
+            {
+                Name = "Contact 1",
+                Email = "contact1@test.com"
+            },
+            new Contact
+            {
+                Name = "Contact 2",
+                Email = "contact2@test.com"
+            },
+            new Contact
+            {
+                Name = "Contact 3",
+                Email = "contact3@test.com"
+            },
+            new Contact
+            {
+                Name = "Contact 4",
+                Email = "contact4@test.com"
+            },
+            new Contact
+            {
+                Name = "Contact 5",
+                Email = "contact5@test.com"
+            }
+        };
+    }
+}
